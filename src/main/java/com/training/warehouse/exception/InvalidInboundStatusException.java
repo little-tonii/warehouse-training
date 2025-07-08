05a53fb462c07c85cd6be3abc5ae304531f7078e
spleeter <ngdnam03@gmail.com>
2025-07-08 14:59:12 +0700

    add custom exception

--- src/main/java/com/training/warehouse/exception/InvalidInboundStatusException.java
+++ src/main/java/com/training/warehouse/exception/InvalidInboundStatusException.java
@@ -1,7 +1,7 @@
 package com.training.warehouse.exception;
 
 public class InvalidInboundStatusException extends RuntimeException {
-  public InvalidInboundStatusException(String message) {
-    super(message);
-  }
+    public InvalidInboundStatusException(String message) {
+        super(message);
+    }
 }
