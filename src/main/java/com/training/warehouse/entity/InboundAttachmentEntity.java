06725b70699838a1277ae7df4687372487d27e88
little-tonii <laptrinhcungtony@gmail.com>
2025-07-11 16:21:40 +0700

    feat: update entity

---
+++ src/main/java/com/training/warehouse/entity/InboundAttachmentEntity.java
@@ -0,0 +1,27 @@
+package com.training.warehouse.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "inbound_attachment")
+@Entity
+public class InboundAttachmentEntity extends BaseEntity{
+
+    @Column(nullable = false, name = "file_name")
+    private String fileName;
+
+    @Column(nullable = false, name = "inb_id")
+    private long inboundId;
+    
+}
