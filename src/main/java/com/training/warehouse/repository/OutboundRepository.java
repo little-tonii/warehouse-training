427d2402528472f196045ac65595db4c3c47e735
little-tonii <laptrinhcungtony@gmail.com>
2025-07-16 10:37:17 +0700

    feat: excel service

--- src/main/java/com/training/warehouse/repository/OutboundRepository.java
+++ src/main/java/com/training/warehouse/repository/OutboundRepository.java
@@ -1,13 +1,23 @@
 package com.training.warehouse.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.training.warehouse.entity.OutboundEntity;
 
 @Repository
-public interface OutboundRepository extends JpaRepository<OutboundEntity, Long>{
+public interface OutboundRepository extends JpaRepository<OutboundEntity, Long> {
     List<OutboundEntity> findByInboundId(long inboundId);
+
+    @Query("""
+                SELECT outbound
+                FROM OutboundEntity outbound
+                WHERE (outbound.actualShippingDate IS NULL OR outbound.actualShippingDate > outbound.expectedShippingDate)
+                  AND outbound.createdAt BETWEEN :from AND :to
+            """)
+    List<OutboundEntity> findLateOutboundsCreatedBetween(LocalDateTime from, LocalDateTime to);
 }
