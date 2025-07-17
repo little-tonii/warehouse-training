57d5fd1b17ece58563a895abe494657856c038d3
spleeter <ngdnam03@gmail.com>
2025-07-17 15:24:36 +0700

    fix: change pricipal to UserEntity

--- src/main/java/com/training/warehouse/common/util/SecurityUtil.java
+++ src/main/java/com/training/warehouse/common/util/SecurityUtil.java
@@ -1,6 +1,5 @@
 package com.training.warehouse.common.util;
 
-import com.training.warehouse.entity.CustomUserDetails;
 import com.training.warehouse.entity.UserEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -9,14 +8,10 @@ public class SecurityUtil {
     private SecurityUtil() {
     }
 
-    public static CustomUserDetails getCurrentUserDetails() {
-        return (CustomUserDetails) SecurityContextHolder
+    public static UserEntity getCurrentUser() {
+        return (UserEntity) SecurityContextHolder
                 .getContext()
                 .getAuthentication()
                 .getPrincipal();
-    }
-
-    public static UserEntity getCurrentUser() {
-        return getCurrentUserDetails().getUserEntity();
     }
 }
