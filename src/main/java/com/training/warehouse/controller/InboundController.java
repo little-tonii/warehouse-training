ee23baf994b301cc082b26d8cc729deb3dacc68b
spleeter <ngdnam03@gmail.com>
2025-07-29 16:09:23 +0700

    feat: RPT-02

--- src/main/java/com/training/warehouse/controller/InboundController.java
+++ src/main/java/com/training/warehouse/controller/InboundController.java
@@ -1,5 +1,8 @@
 package com.training.warehouse.controller;
 
+import com.training.warehouse.service.InboundStatisticService;
+import com.training.warehouse.service.impl.ExcelServiceImpl;
+import com.training.warehouse.service.impl.InboundStatisticServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,6 +19,45 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 
+import com.training.warehouse.dto.response.InboundSummaryMonthProjection;
+import com.training.warehouse.dto.response.InboundSummaryPerMonth;
+import com.training.warehouse.dto.response.InboundSummaryResponse;
+import com.training.warehouse.exception.BadRequestException;
+
+
+import jakarta.validation.constraints.Max;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.io.ByteArrayOutputStream;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Year;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import jakarta.validation.constraints.Min;
 @Controller
 @RequestMapping("/api/inbound")
 @AllArgsConstructor
@@ -23,7 +65,7 @@ import lombok.AllArgsConstructor;
 @Validated
 public class InboundController {
     private final InboundService inboundService;
-
+    private final InboundStatisticService inboundStatisticService;
 
     @io.swagger.v3.oas.annotations.Operation(
         method = "DELETE",
@@ -82,5 +124,177 @@ public class InboundController {
     public ResponseEntity<?> deleteById(@PathVariable @Min(value = 1, message = "inboundId must be greater than 0") long id) {
         inboundService.deleteInboundById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @Operation(
+            method = "GET",
+            summary = "get inbound summary group by productType and supplierCd",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Get inbound summary success",
+                            content =
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = InboundSummaryResponse.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request – Invalid ID format or logic conflict",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized – Missing or invalid token",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden – Not allowed to delete this record",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not found – Inbound record does not exist",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error – Unexpected issue",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionResponse.class)
+                            )
+                    )
+            }
+
+    )
+    @GetMapping("/inbound-summary")
+    public Page<InboundSummaryResponse> getInboundSummary(@ParameterObject Pageable pageable) {
+        return inboundStatisticService.getInboundSummary(pageable);
+    }
+
+    @Operation(
+            parameters = {
+                    @Parameter(name = "startMonth", example = "3" ,schema = @Schema(type = "integer")),
+                    @Parameter(name = "endMonth", example = "5",schema = @Schema(type = "integer")),
+                    @Parameter(name = "year", example = "2025",schema = @Schema(type = "integer"))
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                    schema = @Schema(type = "string", format = "binary")
+                            ))
+            })
+    @GetMapping(value = "/inbound-summary-by-month",
+            produces = "application/xlsx")
+    public ResponseEntity<byte[]> getInboundSummaryByMonth(
+            @RequestParam(name = "startMonth", defaultValue = "1") @Min(1) @Max(12) int startMonth,
+            @RequestParam(name = "endMonth", defaultValue = "12") @Min(1) @Max(12) int endMonth,
+            @RequestParam(name = "year") Integer year) {
+
+        System.out.println("startMonth = " + startMonth);
+        System.out.println("endMonth = " + endMonth);
+        System.out.println("year = " + year);
+        if (startMonth > endMonth) {
+            throw new BadRequestException("Start month cannot be greater than end month");
+        }
+        if (year == null) {
+            year = Year.now().getValue();
+        }
+        List<InboundSummaryMonthProjection> statisticData = inboundStatisticService.getInboundSummaryByMonth(startMonth, endMonth, year);
+
+        System.out.println(statisticData);
+        Map<Integer, List<InboundSummaryPerMonth>> groupdDataByMonth = InboundStatisticServiceImpl.extractAndGroupDataByMonth(statisticData);
+        try (XSSFWorkbook workbook = ExcelServiceImpl.createSummaryWorkbook(groupdDataByMonth);
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            XSSFSheet sheet = workbook.getSheet("summary");
+
+            int col1 = 4, col2 = 14;
+            int idx = 1;
+            int firstRow = -1;
+            int lastRow = sheet.getLastRowNum();
+            Integer currentMonth = null;
+            int chartBaseRow = 1;
+
+            while (idx <= lastRow) {
+                Row row = sheet.getRow(idx);
+                if (row == null) {
+                    idx++;
+                    continue;
+                }
+
+                Cell cell = row.getCell(0);
+                if (cell != null && cell.getCellType() == CellType.NUMERIC) {
+                    int month = (int) cell.getNumericCellValue();
+
+                    if (currentMonth != null && !Objects.equals(currentMonth, month)) {
+                        int endRow = idx - 1;
+                        int chartStartRow = chartBaseRow;
+                        int chartEndRow = chartStartRow + 12;
+
+                        ExcelServiceImpl.drawBarChart(
+                                workbook,
+                                firstRow, endRow,
+                                col1, chartStartRow,
+                                col2, chartEndRow,
+                                currentMonth
+                        );
+
+                        chartBaseRow = chartEndRow + 2;
+                        firstRow = idx;
+                    } else if (currentMonth == null) {
+                        firstRow = idx;
+                    }
+                    currentMonth = month;
+                }
+                idx++;
+            }
+
+            if (currentMonth != null && firstRow <= lastRow) {
+                int endRow = lastRow;
+                int chartStartRow = chartBaseRow;
+                int chartEndRow = chartStartRow + 9;
+
+                ExcelServiceImpl.drawBarChart(
+                        workbook,
+                        firstRow, endRow,
+                        col1, chartStartRow,
+                        col2, chartEndRow,
+                        currentMonth
+                );
+            }
+
+            workbook.write(out);
+            byte[] fileContent = out.toByteArray();
+
+            String fileName = URLEncoder.encode("InboundSummary-RPT2.xlsx", StandardCharsets.UTF_8);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(
+                    MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+            headers.setContentDisposition(ContentDisposition.attachment().filename(fileName).build());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(fileContent.length)
+                    .body(fileContent);
+        } catch (Exception e) {
+            throw new RuntimeException("lỗi: "+e);
+        }
     }
 }
--- src/main/java/com/training/warehouse/service/impl/ExcelServiceImpl.java
+++ src/main/java/com/training/warehouse/service/impl/ExcelServiceImpl.java
@@ -24,6 +24,17 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import com.training.warehouse.service.ExcelService;
+import com.training.warehouse.dto.response.InboundSummaryPerMonth;
+import com.training.warehouse.enumeric.ProductType;
+
+import org.apache.poi.xddf.usermodel.PresetColor;
+import org.apache.poi.xddf.usermodel.XDDFColor;
+import org.apache.poi.xddf.usermodel.XDDFShapeProperties;
+import org.apache.poi.xddf.usermodel.XDDFSolidFillProperties;
+import org.apache.poi.xddf.usermodel.chart.*;
+import org.apache.poi.xssf.usermodel.*;
+
+import java.util.*;
 
 @Service
 public class ExcelServiceImpl implements ExcelService {
@@ -90,5 +101,125 @@ public class ExcelServiceImpl implements ExcelService {
         XDDFChartData.Series series = pieData.addSeries(categories, values);
         series.setTitle("Distribution", null);
         chart.plot(pieData);
+    }
+
+    public static XSSFWorkbook createSummaryWorkbook(Map<Integer, List<InboundSummaryPerMonth>> groupedData) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet summarySheet = workbook.createSheet("Summary");
+        ExcelServiceImpl.clearSheet(summarySheet);
+        int rowIdx = 0;
+
+        Row header = summarySheet.createRow(rowIdx++);
+        header.createCell(0).setCellValue("Month");
+        header.createCell(1).setCellValue("Supplier");
+        header.createCell(2).setCellValue("Aircon");
+        header.createCell(3).setCellValue("SparePart");
+
+        Set<Integer> month = new HashSet<>();
+        for (Map.Entry<Integer, List<InboundSummaryPerMonth>> partDataEntry : groupedData.entrySet()) {
+            List<InboundSummaryPerMonth> inboundSummaryPerMonthsList = partDataEntry.getValue();
+
+            int monthKey = partDataEntry.getKey();
+            Map<String, Map<String, Long>> quantityPerMonth = InboundStatisticServiceImpl.groupDataBySupplierAndProductType(inboundSummaryPerMonthsList);
+
+            for (Map.Entry<String, Map<String, Long>> supplierEntry : quantityPerMonth.entrySet()) {
+                Map<String, Long> productMap = supplierEntry.getValue();
+                String supplierCdKey = supplierEntry.getKey();
+                Row row = summarySheet.createRow(rowIdx++);
+                if (!month.contains(monthKey)) {
+                    month.add(monthKey);
+                    row.createCell(0).setCellValue(monthKey);
+                }
+
+                long quantityAircon = productMap.getOrDefault(ProductType.AIRCON.getName(), 0L);
+                long quantitySpare = productMap.getOrDefault(ProductType.SPARE_PART.getName(), 0L);
+
+                row.createCell(1).setCellValue(supplierCdKey);
+                row.createCell(2).setCellValue((double) quantityAircon);
+                row.createCell(3).setCellValue((double) quantitySpare);
+            }
+        }
+        return workbook;
+    }
+
+    public static void drawBarChart(Workbook workbook,int firstRow, int endRow,int col1, int row1,int col2,int row2, int month){
+        XSSFSheet sheet = (XSSFSheet) workbook.getSheet("Summary");
+        XSSFDrawing drawing = sheet.createDrawingPatriarch();
+        XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, col1, row1, col2, row2);
+
+        XSSFChart chart = drawing.createChart(anchor);
+        chart.getOrAddLegend().setPosition(LegendPosition.RIGHT);
+
+        chart.setTitleText("Quantity Chart month "+ month);
+        chart.setTitleOverlay(false);
+
+        XDDFCategoryAxis xAxis = chart.createCategoryAxis(AxisPosition.LEFT);
+        xAxis.setTitle("Country");
+        chart.setTitleOverlay(false);
+
+        XDDFValueAxis yAxis = chart.createValueAxis(AxisPosition.BOTTOM);
+        yAxis.setTitle("Quantity");
+
+        // Tạo cột ảo và thêm dữ liệu
+        List<String> extendedCountries = new ArrayList<>();
+        List<Double> extendedAirconData = new ArrayList<>();
+        List<Double> extendedSparePartData = new ArrayList<>();
+
+        extendedCountries.add("");
+        extendedAirconData.add(0.0);
+        extendedSparePartData.add(0.0);
+
+        for (int i = firstRow; i <= endRow; i++) {
+            Row row = sheet.getRow(i);
+            if (row != null) {
+                String country = row.getCell(1) != null ? row.getCell(1).getStringCellValue() : "";
+                Double aircon = row.getCell(2) != null ? row.getCell(2).getNumericCellValue() : 0.0;
+                Double sparePart = row.getCell(3) != null ? row.getCell(3).getNumericCellValue() : 0.0;
+                extendedCountries.add(country);
+                extendedAirconData.add(aircon);
+                extendedSparePartData.add(sparePart);
+            }
+        }
+
+        extendedCountries.add("");
+        extendedAirconData.add(0.0);
+        extendedSparePartData.add(0.0);
+
+        XDDFDataSource<String> countries = XDDFDataSourcesFactory.fromArray(extendedCountries.toArray(new String[0]));
+        XDDFNumericalDataSource<Double> airconData = XDDFDataSourcesFactory.fromArray(extendedAirconData.toArray(new Double[0]));
+        XDDFNumericalDataSource<Double> sparePartData = XDDFDataSourcesFactory.fromArray(extendedSparePartData.toArray(new Double[0]));
+        //  //
+
+        XDDFChartData data = chart.createData(ChartTypes.BAR, xAxis, yAxis);
+        // cột đứng
+        XDDFBarChartData bar = (XDDFBarChartData) data;
+        bar.setBarDirection(BarDirection.COL);
+        bar.setBarGrouping(BarGrouping.CLUSTERED);
+
+        XDDFChartData.Series airconSeries = data.addSeries(countries, airconData);
+        XDDFChartData.Series sparePartSeries = data.addSeries(countries, sparePartData);
+
+        airconSeries.setTitle("Aircon", null);
+        sparePartSeries.setTitle("Spare Part", null);
+
+        XDDFShapeProperties airconProps = new XDDFShapeProperties();
+        airconProps.setFillProperties(new XDDFSolidFillProperties(XDDFColor.from(PresetColor.BLUE)));
+        airconSeries.setShapeProperties(airconProps);;
+
+        XDDFShapeProperties spareProps = new XDDFShapeProperties();
+        spareProps.setFillProperties(new XDDFSolidFillProperties(XDDFColor.from(PresetColor.ORANGE)));
+        sparePartSeries.setShapeProperties(spareProps);
+
+        chart.plot(data);
+    }
+
+    public static void clearSheet(XSSFSheet sheet) {
+        int lastRow = sheet.getLastRowNum();
+        for (int i = lastRow; i >= 0; i--) {
+            XSSFRow row = sheet.getRow(i);
+            if (row != null) {
+                sheet.removeRow(row);
+            }
+        }
     }
 }
