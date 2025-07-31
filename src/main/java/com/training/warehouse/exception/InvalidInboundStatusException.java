8766cd124467beb808c9d924e6ca4d8e3e79276d
little-tonii <laptrinhcungtony@gmail.com>
2025-07-31 11:48:51 +0700

    feat: delete the bad file

--- src/main/java/com/training/warehouse/exception/InvalidInboundStatusException.java
+++
@@ -1,7 +0,0 @@
-package com.training.warehouse.exception;
-
-public class InvalidInboundStatusException extends RuntimeException {
-    public InvalidInboundStatusException(String message) {
-        super(message);
-    }
-}
